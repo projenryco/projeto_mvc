e7e3c2c1ae831f7385b08b2d5849da9429edeb68
projetoscintia <enryco.ferreira@etec.sp.gov.br>
2023-07-26 14:49:10 -0300

    primeira versão do projeto

---
+++ cadastrocliente/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastrocliente.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
